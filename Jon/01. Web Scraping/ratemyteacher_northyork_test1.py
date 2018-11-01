c1efcb2e5ad229e03c8d3f4e2da93b05e7f10db4
jonyiukm <43519316+jonyiukm@users.noreply.github.com>
2018-11-01 02:42:00 -0400

    AY Jackson test

---
+++ Jon/01. Web Scraping/ratemyteacher_northyork_test1.py
@@ -0,0 +1,29 @@
+#Grab the necessary information for AY Jackson
+
+import urllib
+from bs4 import BeautifulSoup
+from urllib.request import Request, urlopen
+req = Request('https://ca.ratemyteachers.com/ontario/north-york', headers={'User-Agent':'Mozilla/5.0'})
+webpage = urlopen(req).read()
+
+#If lxml doesn't work, use html.parser instead
+soup = BeautifulSoup(webpage,'lxml')
+
+#Prettify() is meant to print the html nicely (can index like [:1000] for 1000 characters)
+soup.prettify()
+
+#DO NOT ASSIGN A NEW VARIABLE TO SOUP.PRETTIFY SINCE THAT WILL CHANGE THE TYPE TO STR
+#NO FIND FOR STR TYPE
+#USE TYPE(var_name) TO CHECK VARIABLE TYPE
+#USE DIR(var_name) TO CHECK FUNCTIONS AVAILABLE FOR THAT VARIABLE TYPE
+
+#school name is stored under h3 tag
+name_tag = soup.find('h3',attrs={'class': 'school_name'})
+
+print(name_tag)
+
+name_AY = name_tag.text.strip()
+
+print(name_AY)
+
+#next step get all the ratings
